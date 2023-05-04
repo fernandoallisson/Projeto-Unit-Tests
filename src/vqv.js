dfa70cd917a574016a106ebc678b7c53cfa370dd
Fernando Álisson <fernando.alisson@ufpe.br>
2023-05-04 13:48:11 -0300

    Realização de exercício 3 do projeto

--- src/vqv.js
+++ src/vqv.js
@@ -18,6 +18,15 @@
   Caso a função seja chamada sem nenhum parâmetro, o valor undefined deve ser retornado.
 */
 
-const vqv = () => {};
+const vqv = (nome, idade) => {
+  if (typeof nome === 'undefined' || typeof idade === 'undefined') {
+    return undefined;
+  } return `Oi, meu nome é ${nome}!
+Tenho ${idade} anos,
+trabalho na Trybe e mando muito em programação!
+#VQV!`;
+};
+
+console.log(vqv('amigão', 29));
 
 module.exports = vqv;
